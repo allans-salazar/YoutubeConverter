7c7a913482ae6534d8b5aafef1c9bb3b7543f17f
allans-salazar <asalazar22474@gmail.com>
2024-03-28 23:17:45 -0400

    Changed Code to No GUI Class
    Had to scratch the whole MyGUI Class as I was having trouble accessing and updating the loading bar widget outside of the class. I needed the download button function to take the download_video function and use the pytube output_path function. Which gives me the information on the total size of the file and the remaining bytes of transfer and such. Which I use to increment the loading bar with those values. Other than that I got a progress bar to show and work.

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import tkinter
 from tkinter import filedialog
 from tkinter import ttk
 
-""" FUNCTIONS """
+
+""" YOUTUBE FUNCTIONS """
 def download_video(url, save_path): #Youtube function downloads the video & saves to folder path 
     try:
-        yt = YouTube(url)
+        yt = YouTube(url, on_progress_callback=on_progress)
         streams = yt.streams.filter(progressive = True, file_extension="mp4")
         highest_res_stream = streams.get_highest_resolution()
         highest_res_stream.download(output_path=save_path)
@@ -15,76 +16,79 @@ def download_video(url, save_path): #Youtube function downloads the video & save
     except Exception as e: 
         print(e)
 
+def on_progress(stream, total_size, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_completed = round(bytes_downloaded / total_size * 100)
+    loadingBar['value'] = percentage_completed
+    main_window.update()
+    print(percentage_completed)
+
+
 def open_file_dialog(): #function from tkinter to open Folders from PC 
     folder = filedialog.askdirectory()
     if folder:
         print(f"Selected folder: {folder}")
     return folder
 
-"""FUNCTIONS END"""
 
-class MyGUI:
-    def __init__(self):
-        self.main_window = tkinter.Tk()
+""" GUI Functions """
+def downloadButton():
+    link = entry.get()
+    save_dir = open_file_dialog()
 
-        self.main_window.title("Youtube MP4 Converter")
-        self.main_window.geometry("400x150")
-
-        #Displays Label (Instructions) on top of the screen & Entry (Type in the Link) 
-        self.top_frame = tkinter.Frame(self.main_window)
-        self.label = tkinter.Label(self.top_frame, text = 'Enter YouTube link: ')
-        self.entry = tkinter.Entry(self.top_frame, width = 35)
-        self.label.pack()
-        self.entry.pack()
-
-        #Displays MP4 Button 
-        self.bottom_frame = tkinter.Frame(self.main_window)
-        self.mp4Button = tkinter.Button(self.bottom_frame, text = 'CONVERT TO MP4', command = lambda:[self.downloadButton(), self.messageDissapear()])
-        self.mp4Button.pack()
-
-        #Progress Bar
-        self.barFrame = tkinter.Frame(self.main_window)
-        self.loadingBar = ttk.Progressbar(self.barFrame, orient='horizontal', mode='determinate', length=280)
-        self.loadingBar.pack(pady = 15)
+    if save_dir:
+        print("Started download...")
+        download_video(link, save_dir)
+        x.set('Completed Download!')
+    else:
+        print("Invalid save location.")
 
 
-        #Invisible Frame with Label to pop up when download has completed
-        self.result_frame = tkinter.Frame(self.main_window)
-        self.x = tkinter.StringVar()
-        self.show = tkinter.Label(self.result_frame, textvariable = self.x)
-        self.show.pack()
-
-        #Packs all the frames I used
-        self.top_frame.pack()
-        self.bottom_frame.pack()
-        self.barFrame.pack()
-        self.result_frame.pack()
-        
-
-        
-        tkinter.mainloop()
+def out(): #To remove the result frame message
+    x.set(' ')
+    loadingBar['value'] = 0
+def messageDissapear():
+    entry.delete(0, 'end')
+    main_window.after(7000,out)
 
 
-    """ GUI Functions """
-    def downloadButton(self):
-        link = self.entry.get()
-        save_dir = open_file_dialog()
 
-        if save_dir:
-            print("Started download...")
-            download_video(link, save_dir)
-            self.x.set('Completed Download!')
-        else:
-            print("Invalid save location.")
+""" GUI Window """
+main_window = tkinter.Tk()
+
+main_window.title("Youtube MP4 Converter")
+main_window.geometry("400x150")
+
+#Displays Label (Instructions) on top of the screen & Entry (Type in the Link) 
+top_frame = tkinter.Frame(main_window)
+label = tkinter.Label(top_frame, text = 'Enter YouTube link: ')
+entry = tkinter.Entry(top_frame, width = 35)
+label.pack()
+entry.pack()
+
+#Displays MP4 Button 
+bottom_frame = tkinter.Frame(main_window)
+mp4Button = tkinter.Button(bottom_frame, text = 'CONVERT TO MP4', command = lambda:[downloadButton(), messageDissapear()])
+mp4Button.pack()
+
+#Progress Bar
+barFrame = tkinter.Frame(main_window)
+loadingBar = ttk.Progressbar(barFrame, orient='horizontal', mode='determinate', length=280)
+loadingBar.pack(pady = 15)
 
 
-    def out(self): #To remove the result frame message
-        self.x.set(' ')
-    def messageDissapear(self):
-        self.entry.delete(0, 'end')
-        self.main_window.after(9000,self.out)
-        
+#Invisible Frame with Label to pop up when download has completed
+result_frame = tkinter.Frame(main_window)
+x = tkinter.StringVar()
+show = tkinter.Label(result_frame, textvariable = x)
+show.pack()
+
+#Packs all the frames I used
+top_frame.pack()
+bottom_frame.pack()
+barFrame.pack()
+result_frame.pack()
 
 
-#main code
-mygui = MyGUI()
+tkinter.mainloop()
